289c0476563e4372ab8f78fcb6c63a4dad357268
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-22 13:31:35 -0300

    corrigindo as cores dos tipos

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const container = document.getElementById('container');
 const numeroDePokemons = 150;
 const cores = {
-    fire: '#A81E02',
-    grass: '#85C125',
-    electric: '#FEFF01',
-    water: '#2D3192',
-    ground: '#BD4612',
-    fairy: '#FF669C',
-    rock: '#5D6162',
-    poison: '#741F7C',
-    bug: '#FB151F',
-    dragon: '#FC6D21',
-    psychic: '#044318',
+    fire: '#EE8130',
+    grass: '#7AC74C',
+    electric: '#F7D02C',
+    water: '#6390F0',
+    ground: '#E2BF65',
+    fairy: '#D685AD',
+    rock: '#B6A136',
+    poison: '#A33EA1',
+    bug: '#A6B91A',
+    dragon: '#6F35FC',
+    psychic: '#F95587',
     flying: '#79DAFD',
-    fighting: '#C6AC01',
-    normal: '#563317'
+    fighting: '#C22E28',
+    normal: '#A8A77A'
 }
 
 const tipos = Object.keys(cores);
